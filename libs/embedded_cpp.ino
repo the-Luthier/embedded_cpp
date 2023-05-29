e1fb792fad5ed104b31d631b5391a286abdd2e68
Onur KAVRIK <80355788+the-Luthier@users.noreply.github.com>
2023-05-29 16:23:04 +0300

    1234

--- libs/embedded_cpp.ino
+++ libs/embedded_cpp.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LidarLite_v4.h>
 
+void setup() {
+  // Initialize the serial port.
+  Serial.begin(9600);
+
+  // Initialize the Lidar sensor.
+  lidar.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Read the distance from the Lidar sensor.
+  int distance = lidar.distance();
 
+  // Print the distance to the console.
+  Serial.println(distance);
+
+  // If the distance is less than 100 cm, turn on the LED.
+  if (distance < 100) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+
+  // Delay for 1 second.
+  delay(1000);
 }
